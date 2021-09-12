b59ba0603ff9c7a5b4a7531d49d53c410b853638
Strencher <46447572+Strencher@users.noreply.github.com>
2021-09-12 19:56:24 +0200

    Add process global.

--- preload/src/index.js
+++ preload/src/index.js
@@ -2,10 +2,12 @@ import {contextBridge} from "electron";
 import * as BdApi from "./modules/api";
 import path from "path";
 import Module from "module";
+import process from "./modules/process";
 
 Module.globalPaths.push(path.resolve(process.env.DISCORD_APP_PATH, "..", "app.asar", "node_modules"));
 
 contextBridge.exposeInMainWorld("BetterDiscord", BdApi);
+contextBridge.exposeInMainWorld("process", process);
 
 import("./modules/loadDiscord.js");
 
--- preload/src/modules/process.js
+++ preload/src/modules/process.js
@@ -1,3 +1,28 @@
-import IPCEvents from "common/constants/ipcevents";
+import NodeEvents from "events";
 
-export 
+const originalProcess = process;
+
+const cloneObject = function (target, newObject = {}, keys) {
+    if (!Array.isArray(keys)) keys = Object.keys(Object.getOwnPropertyDescriptors(target));
+    return keys.reduce((clone, key) => {
+        if (typeof(target[key]) === "object" && !Array.isArray(target[key]) && target[key] !== null && !(target[key] instanceof NodeEvents)) clone[key] = cloneObject(target[key], {});
+        else clone[key] = target[key];
+
+        return clone;
+    }, newObject);
+};
+
+process = new class process extends NodeEvents {
+    // get __ORIGINAL_PROCESS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED__() {return originalProcess;}
+
+    constructor() {
+        super();
+
+        Object.assign(this,
+            cloneObject(originalProcess, {}, Object.keys(NodeEvents.prototype)),
+            cloneObject(originalProcess, {})
+        );
+    }
+};
+
+export default process;
